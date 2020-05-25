239ebb2c2b4633a047e21aebb0e5fbe3cd9799c7
sayem <sayem@Mac-Air-17.local>
2020-05-25 16:05:29 -0500

    sjfjf

--- src/CWE89_SQL_Injection/CWE89_SQL_Injection__URLConnection_prepareStatement_81_goodG2B_sayem.java
+++ src/CWE89_SQL_Injection/CWE89_SQL_Injection__URLConnection_prepareStatement_81_goodG2B_sayem.java
@@ -26,6 +26,62 @@ import java.util.logging.Level;
 
 public class CWE89_SQL_Injection__URLConnection_prepareStatement_81_goodG2B extends CWE89_SQL_Injection__URLConnection_prepareStatement_81_base
 {
+  
+    public  CWE89_SQL_Injection__URLConnection_prepareStatement_81_goodG2B(String data ) throws Throwable
+    {
+
+        Connection dbConnection = null;
+        PreparedStatement sqlStatement = null;
+
+        try
+        {
+            /* POTENTIAL FLAW: data concatenated into SQL statement used in prepareStatement() call, which could result in SQL Injection */
+            dbConnection = IO.getDBConnection();
+            sqlStatement = dbConnection.prepareStatement("insert into users (status) values ('updated') where name='"+data+"'");
+
+            Boolean result = sqlStatement.execute();
+
+            if (result)
+            {
+                IO.writeLine("Name, " + data + ", updated successfully");
+            }
+            else
+            {
+                IO.writeLine("Unable to update records for user: " + data);
+            }
+        }
+        catch (SQLException exceptSql)
+        {
+            IO.logger.log(Level.WARNING, "Error getting database connection", exceptSql);
+        }
+        finally
+        {
+            try
+            {
+                if (sqlStatement != null)
+                {
+                    sqlStatement.close();
+                }
+            }
+            catch (SQLException exceptSql)
+            {
+                IO.logger.log(Level.WARNING, "Error closing PreparedStatement", exceptSql);
+            }
+
+            try
+            {
+                if (dbConnection != null)
+                {
+                    dbConnection.close();
+                }
+            }
+            catch (SQLException exceptSql)
+            {
+                IO.logger.log(Level.WARNING, "Error closing Connection", exceptSql);
+            }
+        }
+
+    }
     public void action(String data ) throws Throwable
     {
 
