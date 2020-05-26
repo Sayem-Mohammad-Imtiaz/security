f7f97be199d8fc88f90b3b1814b24e570d90ff0d
sayem <sayem@Mac-Air-17.local>
2020-05-26 08:16:02 -0500

    ddgh

--- src/CWE89_SQL_Injection/extended.java
+++ src/CWE89_SQL_Injection/extended.java
@@ -24,13 +24,7 @@ import java.util.logging.Level;
 
 public class extended extends java.sql
 {
-  static{
-    String data=readLine();
-    dbConnection = IO.getDBConnection();
-            sqlStatement = dbConnection.prepareStatement("insert into users (status) values ('updated') where name='"+data+"'");
-
-            Boolean result = sqlStatement.execute();
-  }
+    private String f;
 
     public void extendedTrue1(String data ) throws Throwable
     {
@@ -43,6 +37,61 @@ public class extended extends java.sql
             /* POTENTIAL FLAW: data concatenated into SQL statement used in prepareStatement() call, which could result in SQL Injection */
             dbConnection = IO.getDBConnection();
             sqlStatement = dbConnection.prepareStatement("insert into users (status) values ('updated') where name='"+data+"'");
+
+
+            if (result)
+            {
+                IO.writeLine("Name, " + data + ", updated successfully");
+            }
+            else
+            {
+                IO.writeLine("Unable to update records for user: " + data);
+            }
+        }
+        catch (SQLException exceptSql)
+        {
+            IO.logger.log(Level.WARNING, "Error getting database connection", exceptSql);
+        }
+        finally
+        {
+            try
+            {
+                if (sqlStatement != null)
+                {
+                    sqlStatement.close();
+                }
+            }
+            catch (SQLException exceptSql)
+            {
+                IO.logger.log(Level.WARNING, "Error closing PreparedStatement", exceptSql);
+            }
+
+            try
+            {
+                if (dbConnection != null)
+                {
+                    dbConnection.close();
+                }
+            }
+            catch (SQLException exceptSql)
+            {
+                IO.logger.log(Level.WARNING, "Error closing Connection", exceptSql);
+            }
+        }
+
+    }
+
+     public void extendedTrue2( ) throws Throwable
+    {
+
+        Connection dbConnection = null;
+        PreparedStatement sqlStatement = null;
+
+        try
+        {
+            /* POTENTIAL FLAW: data concatenated into SQL statement used in prepareStatement() call, which could result in SQL Injection */
+            dbConnection = IO.getDBConnection();
+            sqlStatement = dbConnection.prepareStatement("insert into users (status) values ('updated') where name='"+f+"'");
 
 
             if (result)
