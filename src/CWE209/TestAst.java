dd39778f5c9ecba89746da143e0e9b0e0feb2c95
sayem <sayem@mac-air-017.cs.iastate.edu>
2020-06-14 19:26:37 -0500

    dffh

--- src/CWE209/TestAst.java
+++ src/CWE209/TestAst.java
@@ -9,23 +9,9 @@ public class TestAst
 	//just new method call taint
 	public void true1(int a)
 	{
-		a=a+2;
-		int i;
-		for(i=0;i<10;i++)
+		while(true)
 		{
-			if(i==0)
-				continue;
-		    System.out.print("ghgh");
-		}
-	   try{
-
-	   CustomAdd c=new CustomAdd();
-	   c.CustomAdd_1();
-		}
-		catch(Exception ex)
-		{
-			StringWriter errors = new StringWriter();
-			ex.printStackTrace(new PrintWriter(errors));
+			
 		}
 	}
 	
